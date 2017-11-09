422a1436b9ea58aa395cfd0c87b17a8fd993425d
Cláusio Barbosa <clausiobarbosa@gmail.com>
2017-11-09 20:49:39 -0300

    alter to uint 64

--- keccakhash.c
+++ keccakhash.c
@@ -23,7 +23,7 @@ void keccak_hash(const char* input, char* output)
 
     
     //these uint512 in the c++ source of the client are backed by an array of uint32
-    uint32_t hashA[16], hashB[16];
+    uint64_t hashA[16], hashB[16];
     
     //uint256_t hashA[16], hashB[16];
 
