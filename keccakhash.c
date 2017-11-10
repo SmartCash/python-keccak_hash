286840a18cfedb2ddf34780f27fc3c2b291c91be
rc125 <cruzrenato@gmail.com>
2017-11-09 21:43:56 -0300

    uint5256

--- keccakhash.c
+++ keccakhash.c
@@ -23,9 +23,7 @@ void keccak_hash(const char* input, char* output)
 
     
     //these uint512 in the c++ source of the client are backed by an array of uint32
-    uint64_t hashA[16], hashB[16];
-    
-    //uint256_t hashA[16], hashB[16];
+    uint64_t hashA[32], hashB[32];
 
     sph_keccak256_init(&ctx_keccak);
     sph_keccak256 (&ctx_keccak, hashA, 64);
